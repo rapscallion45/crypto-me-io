086bfc31decb8c1b9d7fb6098bf4e8494e0b983a
Carl <carl.scrivener.89@gmail.com>
2022-05-19 12:02:32 +0100

    update bio section

--- components/currency-bio.js
+++ components/currency-bio.js
@@ -3,7 +3,7 @@ import parse from 'html-react-parser';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Collapse from '@mui/material/Collapse';
-import { Button } from '@mui/material';
+import { Button, Card, CardContent } from '@mui/material';
 
 const CurrencyBio = function CurrencyBio({ data }) {
   const [expanded, setExpanded] = useState(false);
@@ -13,26 +13,28 @@ const CurrencyBio = function CurrencyBio({ data }) {
   };
 
   return (
-    <>
-      <Typography variant="h4" color="secondary.main" paragraph>
-        Bio:
-      </Typography>
-      <Collapse in={expanded} timeout="auto" collapsedSize="80px">
-        <Typography variant="body" color="secondary.main" paragraph>
-          {data && parse(data?.description?.en)}
+    <Card>
+      <CardContent>
+        <Typography variant="h4" color="primary.main" paragraph>
+          Bio:
         </Typography>
-      </Collapse>
-      <Box display="flex" justifyContent="center">
-        <Button
-          variant="text"
-          color="secondary"
-          onClick={handleExpandClick}
-          sx={{ border: 'none' }}
-        >
-          {expanded ? 'Show Less' : 'Show More...'}
-        </Button>
-      </Box>
-    </>
+        <Collapse in={expanded} timeout="auto" collapsedSize="80px">
+          <Typography variant="body" paragraph color="primary">
+            {data && parse(data?.description?.en)}
+          </Typography>
+        </Collapse>
+        <Box display="flex" justifyContent="center">
+          <Button
+            variant="text"
+            color="primary"
+            onClick={handleExpandClick}
+            sx={{ border: 'none' }}
+          >
+            {expanded ? 'Show Less' : 'Show More...'}
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
   );
 };
 
